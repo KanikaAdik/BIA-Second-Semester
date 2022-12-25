f7027d218c7116e12fae3bf9da6b36bc5830c99b
KanikaAdik <kanikaadik07@gmail.com>
2022-12-25 08:53:32 -0500

    adding 652 course by Feng Mai

---
+++ 652 Multivariate Data Analysis I/Week 13/copy_of_glm_in_r.R
@@ -0,0 +1,109 @@
+install.packages('AER')
+install.packages('margins')
+library(AER)
+library(tidyverse)
+library(stargazer)
+data("SmokeBan")
+SmokeBan <-
+  SmokeBan %>% mutate(smoker = ifelse(smoker == 'yes', 1, 0))
+reg_mod <- lm(smoker ~ ., data = SmokeBan)
+reg_mod %>% stargazer(type = 'text')
+SmokeBan$y_pred <- predict(reg_mod, SmokeBan) %>% round(2)
+
+logit_mod <-
+  glm(
+    smoker ~ age + ban + gender + education + afam + hispanic,
+    data = SmokeBan,
+    family = "binomial"
+  )
+stargazer(
+  logit_mod,
+  type = 'text' ,
+  no.space = T,
+  covariate.labels = c(
+    "Age",
+    "Workplace Smoking Ban",
+    "Female",
+    "High school",
+    "Some college",
+    "College",
+    "Master",
+    "African-American",
+    "Hispanic",
+    "Constant"
+  )
+)
+
+library(margins)
+cplot(logit_mod,
+      x = "ban",
+      what = "effect",
+      ylim = c(-1, 0))
+plot(margins(logit_mod, variables = c('ban', 'gender')),
+     labels = c("Workplace Smoking Ban", "Female"))
+
+data("DoctorVisits")
+DoctorVisits <- DoctorVisits %>% mutate(age = age * 100)
+hist(DoctorVisits$visits)
+
+poi_mod <-
+  glm(
+    visits ~ gender + age + income + illness + reduced + health,
+    family = "poisson",
+    data = DoctorVisits
+  )
+
+stargazer(poi_mod,
+          type = 'text',
+          no.space = T)
+
+
+
+"""# Poisson Regression"""
+
+data("DoctorVisits")
+DoctorVisits <- DoctorVisits %>% mutate(age = age * 100)
+hist(DoctorVisits$visits)
+
+poi_mod <-
+  glm(
+    visits ~ gender + age + income + illness + reduced + health,
+    family = "poisson",
+    data = DoctorVisits
+  )
+exp(confint(poi_mod))
+stargazer(poi_mod,
+          type = 'text',
+          no.space = T)
+
+"""# Poisson and Negative Binomial regression"""
+
+fHH1 <-
+  read_csv("https://raw.githubusercontent.com/proback/BeyondMLR/master/data/fHH1.csv")
+
+modela <- glm(total ~ age, family = poisson, data = fHH1)
+confint(modela)
+exp(confint(modela))
+
+model0 <- glm(total ~ 1, family = poisson, data = fHH1)
+
+fHH1 <- fHH1 %>% mutate(age2 = age * age)
+modela2 <- glm(total ~ age + age2, family = poisson,
+               data = fHH1)
+modela2 %>% stargazer(type = 'text', no.space = T)
+
+modela2L <- glm(total ~ age + age2 + location,
+                family = poisson, data = fHH1)
+
+modela2L %>% stargazer(type = 'text', no.space = T)
+
+# Account for over-dispersion with negative binomial model
+hist(fHH1$total)
+library(MASS)
+modelinb <- glm.nb(total ~ age + age2 + location, data = fHH1)
+modelinb %>% stargazer(type = 'text', no.space = T)
+
+model_pos <- glm(total ~ age + age2 + location, family = poisson, data = fHH1)
+
+stargazer(model_pos, modelinb, type = 'text', no.space = T)
+
